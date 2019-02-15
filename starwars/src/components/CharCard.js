27bb068a6bb86f7cfc6bcf648856639324f94b36
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-02-15 10:50:30 -0700

    Created list elements for each character.

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -5,8 +5,14 @@ const CharCard = props => {
     return (
         <div className="each-character">
             <h2>{props.chars.name}</h2>
+            <ul>
+                <li><strong>Born: </strong>{props.chars.birth_year}</li>
+                <li><strong>Height: </strong>{props.chars.height}</li>
+                <li><strong>Skin Color: </strong>{props.chars.skin_color}</li>
+                <li><strong>Gender: </strong>{props.chars.gender}</li>
+            </ul>
         </div>
     );
-}
+};
 
 export default CharCard;
